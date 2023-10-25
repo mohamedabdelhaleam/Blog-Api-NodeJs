93f9b15e88dda3d5043f2ad316310e9251ff2f95
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2023-10-25 18:13:53 +0300

    Add Articles Controllers

--- controllers/article-controller.js
+++ controllers/article-controller.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Article = require("../models/Article");
 
 const getAllArticle = async (req, res, next) => {
   const query = req.query;
@@ -10,13 +11,103 @@ const getAllArticle = async (req, res, next) => {
       .limit(limit)
       .skip(skip);
     res.json({
-        status : "success",
-        data : {Article : article}
+      status: "success",
+      data: { Article: article },
     });
   } catch (error) {
     res.json({
-        status : "error",
-        message : error.message
-    })
+      status: "error",
+      message: error.message,
+    });
   }
+};
+
+const getArticleById = async (req, res, next) => {
+  const id = req.params.articleId;
+  try {
+    const article = await Article.findById(id);
+    res.json({
+      status: "success",
+      data: { Article: article },
+    });
+  } catch (error) {
+    res.json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
+const deleteArticle = async (req, res, next) => {
+  const id = req.params.articleId;
+  try {
+    const article = await Article.deleteOne({ _id: id });
+    res.json({
+      status: "success",
+      message: "Article Deleted Successfully",
+      data: null,
+    });
+  } catch (error) {
+    res.json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
+const updateArticle = async (req, res, next) => {
+  const id = req.params.articleId;
+  try {
+    const article = await Article.updateOne(
+      { _id: id },
+      { $set: { ...req.body } }
+    );
+    console.log(req.body);
+    res.json({
+      status: "success",
+      message: "Article Updated Successfully",
+      data: {
+        Article : article
+      },
+    });
+  } catch (error) {
+    res.json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
+
+const addArticle = async (req,res,next)=>{
+  const title = req.body.title
+  const body = req.body.body
+  const author = req.body.author
+  const likes = req.body.likes
+  try {
+    const newArticle = new Article()
+    newArticle.title = title
+    newArticle.body = body
+    newArticle.author = author
+    newArticle.numberOfLikes = likes
+    newArticle.save()
+    res.json({
+      status: "success",
+      message: "Article Added Successfully",
+      data: {
+        Article : newArticle
+      },
+    });
+    
+  } catch (error) {
+    res.json({
+      status: "error",
+      message: error.message,
+    });
+  }
+}
+
+module.exports = {
+  getAllArticle,
+  getArticleById,
+  addArticle,
+  deleteArticle,
+  updateArticle,
 };
