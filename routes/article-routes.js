78d62bfa60d5488842a3cb34245ecffe575d873a
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2023-11-20 14:10:04 +0200

    Add Validation to title

--- routes/article-routes.js
+++ routes/article-routes.js
@@ -1,16 +1,23 @@
+const express = require("express");
+const { body } = require("express-validator");
 
-const express = require('express')
+const router = express.Router();
+const articleControllers = require("../controllers/article-controller");
 
-const router = express.Router()
-const articleControllers = require("../controllers/article-controller")
+router.get("/getAllArticle", articleControllers.getAllArticle);
 
-router.get("/getAllArticle",articleControllers.getAllArticle)
+router.get("/getAllArticle", articleControllers.getAllArticle);
+router.post(
+  "/addArticle",
+  body("title").notEmpty().escape().isLength({ min: 5, max: 20 }),
+  articleControllers.addArticle
+);
+router.get("/getSingleArticle/:articleId", articleControllers.getArticleById);
+router.delete("/deleteArticle/:articleId", articleControllers.deleteArticle);
+router.patch(
+  "/updateArticle/:articleId",
+  body("title").notEmpty().escape().isLength({ min: 5, max: 20 }),
+  articleControllers.updateArticle
+);
 
-router.get("/getAllArticle",articleControllers.getAllArticle);
-router.post("/addArticle",articleControllers.addArticle);
-router.get("/getSingleArticle/:articleId",articleControllers.getArticleById);
-router.delete("/deleteArticle/:articleId",articleControllers.deleteArticle);
-router.patch("/updateArticle/:articleId",articleControllers.updateArticle);
-
-
-module.exports = router
+module.exports = router;
