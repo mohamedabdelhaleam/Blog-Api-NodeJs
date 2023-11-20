d24a121a1078f999c63391d591c302c9df82a382
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2023-11-20 13:57:01 +0200

    Add Validation to update Article

--- controllers/article-controller.js
+++ controllers/article-controller.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Article = require("../models/Article");
+const {validationResult } = require('express-validator');
 const HttpStatusText = require("../utils/HttpStatusText");
 
 const getAllArticle = async (req, res, next) => {
@@ -26,8 +27,8 @@ const getAllArticle = async (req, res, next) => {
 const getArticleById = async (req, res, next) => {
   const id = req.params.articleId;
   try {
-    const article = await Article.findById({"_id" :id},{"__v":false});
-    if(!article){
+    const article = await Article.findById({ _id: id }, { __v: false });
+    if (!article) {
       res.status(404).json({
         status: HttpStatusText.FAIL,
         data: { Article: article },
@@ -62,16 +63,48 @@ const deleteArticle = async (req, res, next) => {
 };
 const updateArticle = async (req, res, next) => {
   const id = req.params.articleId;
+  const result = validationResult(req)
+  if(result.isEmpty()){
+
+    try {
+      const article = await Article.updateOne(
+        { _id: id },
+        { $set: { ...req.body } }
+      );
+      res.json({
+        status: HttpStatusText.SUCCESS,
+        message: "Article Updated Successfully",
+        data: {
+          Article: article,
+        },
+      });
+    } catch (error) {
+      res.json({
+        status: HttpStatusText.ERROR,
+        message: error.message,
+      });
+    }
+  }
+  res.send({ errors: result.array() });
+};
+
+const addArticle = async (req, res, next) => {
+  const title = req.body.title;
+  const body = req.body.body;
+  const author = req.body.author;
+  const likes = req.body.likes;
   try {
-    const article = await Article.updateOne(
-      { _id: id },
-      { $set: { ...req.body } }
-    );
+    const newArticle = new Article();
+    newArticle.title = title;
+    newArticle.body = body;
+    newArticle.author = author;
+    newArticle.numberOfLikes = likes;
+    newArticle.save();
     res.json({
       status: HttpStatusText.SUCCESS,
-      message: "Article Updated Successfully",
+      message: "Article Added Successfully",
       data: {
-        Article : article
+        Article: newArticle,
       },
     });
   } catch (error) {
@@ -81,34 +114,6 @@ const updateArticle = async (req, res, next) => {
     });
   }
 };
-
-const addArticle = async (req,res,next)=>{
-  const title = req.body.title
-  const body = req.body.body
-  const author = req.body.author
-  const likes = req.body.likes
-  try {
-    const newArticle = new Article()
-    newArticle.title = title
-    newArticle.body = body
-    newArticle.author = author
-    newArticle.numberOfLikes = likes
-    newArticle.save()
-    res.json({
-      status: HttpStatusText.SUCCESS,
-      message: "Article Added Successfully",
-      data: {
-        Article : newArticle
-      },
-    });
-    
-  } catch (error) {
-    res.json({
-      status: HttpStatusText.ERROR,
-      message: error.message,
-    });
-  }
-}
 
 module.exports = {
   getAllArticle,
